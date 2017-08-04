0bc6ad836d28e13fdd018be0d27aa9f057b77a74
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2017-08-03 23:19:37 -0500

    Found out a commit may have more than 1 parent :(

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -2,11 +2,12 @@ class Commit
   include Neo4j::ActiveNode
   id_property :sha
 
-
   property :commit_message, type: String
   property :commit_date, type: DateTime
   property :author_date, type: DateTime
 
-  has_many :in, :childs, origin: :is_child_of, model_class: :Commit
-  has_one :out, :parent, type: :is_child_of,   model_class: :Commit
+  has_many :in,  :childs,  origin: :is_child_of, model_class: :Commit
+  has_many :out, :parents, type: :is_child_of,   model_class: :Commit
 end
+
+# It's not a tree, it's a DAG!
